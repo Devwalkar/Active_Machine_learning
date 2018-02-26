95ec1ca90018574dbf857e73b046fbf10a896619
Devwalkar <Devwalkar64@gmail.com>
2018-02-26 16:35:29 -0500

    Changes to oracle to train iterator calling

--- script_l.py
+++ script_l.py
@@ -22,6 +22,10 @@ from keras.engine import Layer
 from keras.preprocessing.image import ImageDataGenerator, load_img, img_to_array
 from keras.models import load_model
 #from keras.utils.np_utils import probas_to_classes
+######################################################################################################
+Expt_load = 'exp01.hdf5'
+Expt_save = 'exp02.hdf5'
+
 
 from classifier import Classifier
 '''
@@ -29,7 +33,8 @@ FUNCTION DEFINITIONS
 '''
 
 
-def add_file_to_iterator(f, train_it, oracle_it):
+def add_file_to_iterator(f):
+	global train_it,oracle_it,val_it,test_it
 	oracle_file = os.path.join('..', 'oracle', f)
 	c, _ = os.path.split(f)
 	if c == 'pathological':
@@ -56,10 +61,7 @@ def add_file_to_iterator(f, train_it, oracle_it):
 	#oracle_it.index_generator = oracle_it._flow_index(oracle_it.n, oracle_it.batch_size, oracle_it.shuffle, None)
 	#oracle_it.index_generator = oracle_it._flow_index()
 
-	train_it, oracle_it, val_it, test_it = train.get_data_iterators(batch_size=batch_size, horizontal_flip=horizontal_flip,
-																	vertical_flip=vertical_flip, width_shift_range=width_shift_range,
-																	height_shift_range=height_shift_range, rotation_range=rotation_range,
-																	zoom_range=zoom_range, data_dir=img_dir, target_size=(512, 512),
+	train_it, oracle_it, val_it, test_it = train.get_data_iterators(batch_size=8, data_dir='mess_cb', target_size=(512, 512),
 																	rescale=1/255., fill_mode='constant', load_train_data=False, color_mode='rgb')
 
 
@@ -75,7 +77,7 @@ active learning : 	active learning is implemented based on uncertainity sampling
 '''
 ##############################################################################################
 
-def active_learning(pred_prob, oracle_it, train_it, img_dir):
+def active_learning(pred_prob, img_dir):
 
 	names_dict = {}
 	file_index = 0
@@ -102,7 +104,7 @@ def active_learning(pred_prob, oracle_it, train_it, img_dir):
 
 	if max_entrophy > 0.4:
 		active_learning_mode = 1
-		add_file_to_iterator(names_dict[max_entrophy_index], train_it, oracle_it)
+		add_file_to_iterator(names_dict[max_entrophy_index])
 
 	else:
 		active_learning_mode = 0
@@ -117,27 +119,23 @@ def train_and_evaluate(batch_size=8, img_dir='mess_cb',
                        width_shift_range=0.03, height_shift_range=0.03, rotation_range=360,
                        zoom_range=0.03, exp_name='exp0.hdf5', n_blocks=4, context_blocks=1,
                        patience=75):
+
+	global train_it,oracle_it,val_it,test_it				   
 	# Initialization
 	active_learning_mode = 1
-	epochs = 30
+	epochs = 20
 	oracle_len = 568    #71       #568
 	best_loss = 10
 	train_len = 200   #25            #200
 	#test_len = 200
 
-	train_it, oracle_it, val_it, test_it = train.get_data_iterators(batch_size=batch_size, horizontal_flip=horizontal_flip,
-																	vertical_flip=vertical_flip, width_shift_range=width_shift_range,
-																	height_shift_range=height_shift_range, rotation_range=rotation_range,
-																	zoom_range=zoom_range, data_dir=img_dir, target_size=(512, 512),
-																	rescale=1/255., fill_mode='constant', load_train_data=False, color_mode='rgb')
-
-    checkpointer = ModelCheckpoint(filepath= exp_name, monitor = 'val_acc', verbose=1, save_best_only=True, save_weights_only=False)
+	checkpointer = ModelCheckpoint(filepath= exp_name, monitor = 'val_acc', verbose=1, save_best_only=True, save_weights_only=False)
 	early = EarlyStopping(monitor = 'val_acc',patience=2, verbose=1)
 	callbacks = [checkpointer,early]
     #####################################################################################################
     # Loading saved model here
 
-	model = load_model('exp0.hdf5')
+	model = load_model(Expt_load)
 
 	for layer in model.layers:
 		layer.trainable = True
@@ -146,19 +144,21 @@ def train_and_evaluate(batch_size=8, img_dir='mess_cb',
 	model.compile(optimizer=opt, loss='binary_crossentropy', metrics=['acc'])	
 
 	while active_learning_mode:
-		print('Evaluating model {0}:'.format(exp_name))
+		print('Evaluating model {0}:'.format(Expt_load.split('.')[0]))
 		#model.load_weights(exp6.hdf5)
     	#model.load_weights(exp_name)		
 
-		model.fit_generator(train_it, train_len, epochs, validation_data=val_it, validation_steps=192, verbose=2, callbacks=callbacks2)
+		model.fit_generator(train_it, train_len, epochs, validation_data=val_it, validation_steps=192, verbose=2, callbacks=callbacks)
 
 	    # Make sure that you are not shuffling the prediction data in val_it
 		pred_prob = model.predict_generator(oracle_it, oracle_len)
 	
-		active_learning_mode, num_imgs_added = active_learning(pred_prob, oracle_it, train_it, img_dir)
+		active_learning_mode, num_imgs_added = active_learning(pred_prob,img_dir)
 		print('Added {0} images.'.format(num_imgs_added))
 		oracle_len = oracle_len - num_imgs_added
+		print 'Oracle_len:',oracle_len
 		train_len = train_len + num_imgs_added
+		print 'Train_len',train_len
 
 	print('Train AUC = {0}'.format(get_auc_score(model, train_it, train_len)))
 	print('Validation AUC = {0}'.format(get_auc_score(model, val_it, 192)))
@@ -176,30 +176,24 @@ Main Program :  using argparse, add positional arguments. 1st directory should b
 
 if __name__ == '__main__':
 
-    parser = argparse.ArgumentParser(prog='micnn')
-    parser.add_argument('--img_dir', nargs='?', help='Directory where the image data is.', dest='img_dir', default='mess_cb')
-    args = parser.parse_args()
+	parser = argparse.ArgumentParser(prog='micnn')
+	parser.add_argument('--img_dir', nargs='?', help='Directory where the image data is.', dest='img_dir', default='mess_cb')
+	args = parser.parse_args()
 
-    out_sizes = {3: 64, 4: 32, 5: 16}
-    f_sizes = {3: 15, 4: 31, 5: 63}
+	out_sizes = {3: 64, 4: 32, 5: 16}
+	f_sizes = {3: 15, 4: 31, 5: 63}
 
-    experiments = [[3, 0], [3, 1]]
-    for n_blocks in range(3, 6):
-        max_context = 5 - n_blocks
-        for context_blocks in range(max_context + 1):
-            experiments.append([n_blocks, context_blocks])
-    '''	
-    for i, exp in enumerate(experiments):
-        n_blocks, context_blocks = exp
-     
-	
-    	target_size = (512, 512)
-    	classes = ['normal', 'pathological']
-    	color_mode = 'rgb'
-    '''
-    train_and_evaluate(out_size=out_sizes[n_blocks], f_size=f_sizes[n_blocks],
-                           exp_name='exp6.hdf5', n_blocks=n_blocks,
+	experiments = [[3, 0], [3, 1]]
+	for n_blocks in range(3, 6):
+		max_context = 5 - n_blocks
+		for context_blocks in range(max_context + 1):
+			experiments.append([n_blocks, context_blocks])
+
+	train_it,oracle_it,val_it,test_it = train.get_data_iterators(batch_size=8,data_dir='mess_cb',target_size=(512, 512),rescale=1/255,fill_mode='constant',load_train_data=False,color_mode='rgb')
+
+
+	train_and_evaluate(out_size=out_sizes[n_blocks], f_size=f_sizes[n_blocks],
+                           exp_name=Expt_save, n_blocks=n_blocks,
                            context_blocks=context_blocks+1, img_dir=args.img_dir)
-    
     
     
